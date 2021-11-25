f83e291f2d179c407b59c23b4952fb08c3f2a265
andresmunevar@github.com <afmunene@gmail.com>
2021-11-25 07:23:53 -0300

    agregando validacion password parte I

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,33 +1,36 @@
 import React, {useState} from 'react';
 
-const MINEMAIL = 5;
-const MAXEMAIL = 30;
-const GROUPEMAIL = `/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
-//  {${MINEMAIL},${MAXEMAIL}}`;
-// const VALIDATEEMAIL = new RegExp(`^${GROUPEMAIL} (${GROUPEMAIL})*$`);
-const VALIDATEEMAIL = new RegExp(GROUPEMAIL);
-const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
+const MIN_PWD = 8;
+const MAX_PWD = 15;
+const REG_EMAIL = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const REG_PWD_VAR = `^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{${MIN_PWD},${MAX_PWD}}$`;
+const REG_PWD = new RegExp(REG_PWD_VAR);
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [pwd, setPwd] = useState('');
     const [errMail, setErrMail] = useState('');
+    const [errPwd, setErrPwd] = useState('');
 
     const changeHandlerEmail = (event) => {
         setEmail(event.target.value);
-        // const isValid = VALIDATEEMAIL.test(event.target.value);
-        const isValid = re.test(event.target.value.toLowerCase());
-
+        const isValid = REG_EMAIL.test(event.target.value.toLowerCase());
+        
         if (isValid) setErrMail('');
         else setErrMail('Email no valido')
     };
 
-    const changeHandlerPwd = (event) => setPwd(event.target.value);
-    
+    const changeHandlerPwd = (event) => {
+        setPwd(event.target.value);
+        const isValid = REG_PWD.test(event.target.value);
+
+        if (isValid) setErrPwd('');
+        else setErrPwd('Contraseña no valida. Debe tener al menos una mayuscula, una minuscula, un numero y un caracter especial. Su longitud debe estar entre 8 y 15 caracteres')
+    };
+
     const submitHandler = (event) => {
         event.preventDefault();
-        alert('Bienvenido: ' + email);
+        alert(typeof(REG_EMAIL) + "\nY " + REG_PWD);
     };
 
     return (
@@ -37,6 +40,7 @@ const Login = () => {
             {Boolean(errMail) && <p>{errMail}</p>}
             <label htmlFor='pwd'>Contraseña: </label>
             <input type='pwd' name='pwd' value={pwd} onChange={changeHandlerPwd} />
+            {Boolean(errPwd) && <p>{errPwd}</p>}
             <input type='submit' value='Ingresar' />
         </form>
     )
