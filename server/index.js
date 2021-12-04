abca11af3afc14cbafc213aece0b32813270f464
Reinaldo Omar <reymar.mendoza@gmail.com>
2021-12-04 17:20:44 -0500

    DB API base

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const { UserModel } = require("./models/Users")
+
+const app = express()
+
+// de esta forma habilito la opcion de usar json desde express para poder comunicarme con el server
+app.use(express.json())
+app.use(cors())
+
+const USER = "banagrario1"
+const PASSWORD = "eFez3yG5TF9Knj7"
+const DB = "BancoDB"
+
+mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@banagrario.57kdk.mongodb.net/${DB}?retryWrites=true&w=majority`)
+
+app.get("/getUsers", (req, res) => {
+	// despues de encontrar las coincidencias de la coleccion se ejecutara el callback
+	UserModel.find({}, (error, result) => {
+		if (error) {
+			res.json(error)
+		} else {
+			// transforma el resultado a json
+			res.json(result)
+		}
+	})
+})
+
+app.post("/createUser", async (req, res) => {
+	const user = req.body
+	const newUser = new UserModel(user)
+
+	await newUser.save()
+	res.json(user)
+})
+
+// port 3001, react usa el 3000; si sale bien =>
+app.listen(3001, () => {
+	console.log("Server is running")
+})
---
+++ server/models/Users.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+// estandarizar los documentos de la db
+const UserSchema = new mongoose.Schema({
+	// defino cada uno de los campos y sus atributos en la bd
+	name: {
+		type: String,
+		required: true
+	},
+	age: {
+		type: Number,
+		required: true
+	},
+	username: {
+		type: String,
+		required: true
+	}
+})
+
+// (nombre de la coleccion[en la db], esquema que se va a aplicar a ella)
+// si la primera vez que se conecta a la bd la coleccion no existe, se va a crear
+const UserModel = mongoose.model("users", UserSchema)
+
+module.exports = { UserModel }
