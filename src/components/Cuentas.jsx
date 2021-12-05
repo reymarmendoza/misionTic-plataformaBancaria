9b01bb2ebac01803502da9723dfaa7f246ced9b7
Reinaldo Omar <reymar.mendoza@gmail.com>
2021-12-04 21:28:55 -0500

    Pruena W/R a la DB

--- src/components/Cuentas.jsx
+++ src/components/Cuentas.jsx
@@ -1,11 +1,47 @@
-import TransfModal from './TransfModal';
+import TransfModal from './TransfModal'
+import { useEffect, useState } from 'react'
+import Axios from 'axios'
 
 const Cuentas = ({ data }) => {
 	const handleCancelar = (event) => {
-		event.preventDefault();
-		console.log("cancel");
+		event.preventDefault()
+		console.log("cancel")
 	}
 
+	// prueba
+	const [listUsers, setListUsers] = useState([])
+	const [name, setName] = useState('')
+	const [age, setAge] = useState(0)
+	const [username, setUsername] = useState('')
+
+	useEffect(() => {
+		Axios.get("http://localhost:3001/getUsers")
+			.then((response) => {
+				setListUsers(response.data)
+			})
+	}, [])
+
+	const updateNombre = (e) => {
+		setName(e.target.value)
+	}
+	const updateEdad = (e) => {
+		setAge(e.target.value)
+	}
+	const updateUsername = (e) => {
+		setUsername(e.target.value)
+	}
+
+	const handleSenttoDB = () => {
+		Axios.post("http://localhost:3001/createUser", {
+			name: name,
+			age: age,
+			username: username
+		})
+			.then((response) => {
+				console.log(`Usuario creado ${response}`)
+			})
+	}
+	// prueba
 	return (
 		<div>
 			<p>
@@ -38,6 +74,24 @@ const Cuentas = ({ data }) => {
 					}
 				</tbody>
 			</table>
+			{/* Prueba */}
+			{listUsers.map((user) => {
+				return (
+					<div>
+						<h1>{user.name}</h1>
+						<h1>{user.age}</h1>
+						<h1>{user.username}</h1>
+					</div>
+				)
+			})}
+
+			<form>
+				<input type="text" placeholde="nombre" onChange={updateNombre}></input>
+				<input type="number" placeholde="edad" onChange={updateEdad}></input>
+				<input type="text" placeholde="username" onChange={updateUsername}></input>
+				<button type="submit" onClick={handleSenttoDB}></button>
+			</form>
+			{/* Prueba */}
 		</div>
 	)
 };
