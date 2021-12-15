bb92c9324c7e8300b1cf421569c8530d85608ec0
andresmunevar@github.com <afmunene@gmail.com>
2021-12-15 20:54:29 -0300

    reclamos recibe nombre de localStorage

--- src/components/HomePanel.jsx
+++ src/components/HomePanel.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-// import { useLocation } from 'react-router-dom';
-
 
 let HomePanel = () => {
-
-    // const location = useLocation();
 
     return (
         <div className='col-9' >
