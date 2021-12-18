b3b284cf8e8586412c7789b9577f1039cf66f446
Reinaldo Omar <reymar.mendoza@gmail.com>
2021-12-18 16:00:36 -0500

    iconos todo bien

--- src/components/TableSide.jsx
+++ src/components/TableSide.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { Link, useLocation } from 'react-router-dom';
-import RegTransModal from './RegTransModal';
+import { Link, useLocation } from 'react-router-dom'
+import RegTransModal from './RegTransModal'
+// import '../styles/bootstrap-icons.css'
 import '../../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import style from '../styles/tableSide.module.css'
+import { Cuentas } from './Cuentas'
 
 const user = [
 	{
@@ -33,7 +34,7 @@ export function TableSide({ fechaInicio, fechaFin, idCuenta, onUpdate }) {
 								if (s === 'Transferencias') {
 									return (
 										<div className={"d-flex flex-row align-items-center p-1 mb-4 " + style.efecto}>
-											<i className={'bi bi-transferencia ms-3 me-2 text-white'}></i>
+											<i className={'bi bi-arrow-left-right ms-3 me-2 text-white'}></i>
 											<RegTransModal tipoUsr={tipoUsr} s={s}
 												fechaInicio={fechaInicio} fechaFin={fechaFin}
 												idCuenta={idCuenta} onUpdate={onUpdate} />
@@ -41,11 +42,29 @@ export function TableSide({ fechaInicio, fechaFin, idCuenta, onUpdate }) {
 									);
 								}
 								else {
+									let icono = ""
+
+									if (s === "Cuentas") {
+										icono = "clipboard-check"
+									} else if (s === "Reclamos") {
+										icono = "pencil-square"
+									} else if (s === "Nueva Cuenta") {
+										icono = "plus-circle"
+									} else if (s === "Depositos") {
+										icono = "coin"
+									} else if (s === "Cuentas Pendientes") {
+										icono = "journal-check"
+									} else if (s === "Manejo de Cuentas") {
+										icono = "card-checklist"
+									} else if (s === "Gestionar Personal") {
+										icono = "people-fill"
+									}
+
 									return (
 										<li className={"mb-4 " + style.efecto}>
 											<Link to={`/${tipoUsr}/${s.replace(/ /g, '')}`}>
 												<a href={`/${tipoUsr}/${s.replace(/ /g, '')}`} className={'nav-link text-white ' + style.enlace}>
-													<i className={'me-2 bi bi-' + s.replace(/ /g, '')}></i>
+													<i className={`me-2 bi bi-${icono}`}></i>
 													{/* <svg className="bi me-2" width="16" height="16"><use></use></svg> */}
 													{s}
 												</a>
